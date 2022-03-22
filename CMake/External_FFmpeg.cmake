55f41076b7589bcfa67028a75674f0f6b6acb962
Daniel Riehm <daniel.riehm@kitware.com>
2022-03-22 11:44:39 -0400

    Windows changes

--- CMake/External_FFmpeg.cmake
+++ CMake/External_FFmpeg.cmake
@@ -1,90 +1,54 @@
 # If a patch file exists for this version, apply it
 
-if (WIN32)
-  # On windows, FFMPEG relies on prebuilt binaries. These binaries come in two
-  # archives, dev and shared. An external project is added for each of them just
-  # for the download.
-  # The FFmpeg external project then takes care of combining the dev and shared
-  # binaries into the install directory using the Patches/FFmpeg/Install.cmake
-  # script.
-  ExternalProject_Add(FFmpeg_dev
-    URL ${FFmpeg_dev_url}
-    URL_MD5 ${FFmpeg_dev_md5}
-    ${COMMON_EP_ARGS}
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    )
-  fletch_external_project_force_install(PACKAGE FFmpeg_dev)
+set (FFmpeg_patch ${fletch_SOURCE_DIR}/Patches/FFmpeg/${_FFmpeg_version})
+if (EXISTS ${FFmpeg_patch})
+    set(FFMPEG_PATCH_COMMAND ${CMAKE_COMMAND}
+        -DFFmpeg_patch:PATH=${FFmpeg_patch}
+        -DFFmpeg_source:PATH=${fletch_BUILD_PREFIX}/src/FFmpeg
+        -P ${FFmpeg_patch}/Patch.cmake
+        )
+    else()
+    set(FFMPEG_PATCH_COMMAND "")
+endif()
 
-  ExternalProject_Add(FFmpeg_shared
-    URL ${FFmpeg_shared_url}
-    URL_MD5 ${FFmpeg_shared_md5}
-    ${COMMON_EP_ARGS}
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    )
-  fletch_external_project_force_install(PACKAGE FFmpeg_shared)
+if (BUILD_SHARED_LIBS)
+    set(_shared_lib_params --enable-shared --disable-static)
+else()
+    set(_shared_lib_params
+          --disable-shared
+          --enable-static
+          --enable-pic
+          --extra-cflags=-fPIC
+          --extra-cxxflags=-fPIC
+          --disable-asm
+        )
+endif()
 
-  # directly install prebuilt-binaries and shared libraries on windows
-  set (FFmpeg_patch "${fletch_SOURCE_DIR}/Patches/FFmpeg/win32")
-  ExternalProject_Add(FFmpeg
-    DEPENDS "FFmpeg_dev;FFmpeg_shared"
-    DOWNLOAD_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ${CMAKE_COMMAND}
-    -DFFmpeg_dev_SOURCE=${fletch_BUILD_PREFIX}/src/FFmpeg_dev
-    -DFFmpeg_shared_SOURCE=${fletch_BUILD_PREFIX}/src/FFmpeg_shared
-    -DFFmpeg_INSTALL=${fletch_BUILD_INSTALL_PREFIX}
-    -DFFmpeg_PATCH=${FFmpeg_patch}
-    -P "${FFmpeg_patch}/Install.cmake"
-    # no patch command on windows
-    )
-  fletch_external_project_force_install(PACKAGE FFmpeg)
-
-else ()
-  if(fletch_ENABLE_x264)
+if(fletch_ENABLE_x264)
     list(APPEND ffmpeg_DEPENDS x264)
     set(_FFmpeg_x264 --enable-libx264 --enable-gpl)
-  endif()
+endif()
 
-  include(External_yasm)
-  set(fletch_YASM ${fletch_BUILD_PREFIX}/src/yasm-build/yasm)
-  set(_FFmpeg_yasm --yasmexe=${fletch_YASM})
-  list(APPEND ffmpeg_DEPENDS yasm)
+set(FFMPEG_PKGCONFIG_PATH ${fletch_BUILD_INSTALL_PREFIX}/lib/pkgconfig)
+if(WIN32)
+    list(APPEND ffmpeg_DEPENDS msys2)
+    set(FFMPEG_COMMAND_PREFIX ${mingw_prefix} ${msys_bash})
+    set(FFMPEG_BUILD_COMMAND ${FFMPEG_COMMAND_PREFIX} -c make -j 8)
+    set(FFMPEG_INSTALL_COMMAND ${FFMPEG_COMMAND_PREFIX} -c make install)
+    set(FFMPEG_SET_PKG_CONFIG ${mingw_prefix} PKG_CONFIG_PATH=${FFMPEG_PKGCONFIG_PATH} ${msys_bash})
+    execute_process(
+      COMMAND ${mingw_prefix} ${msys_bash} -c "cygpath ${FFMPEG_PKGCONFIG_PATH}"
+      OUTPUT_VARIABLE FFMPEG_PKGCONFIG_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
+else()
+    Fletch_Require_Make()
+    set(FFMPEG_BUILD_COMMAND ${MAKE_EXECUTABLE})
+    set(FFMPEG_INSTALL_COMMAND ${MAKE_EXECUTABLE} install )
+    set(FFMPEG_SET_PKG_CONFIG PKG_CONFIG_PATH=${FFMPEG_PKGCONFIG_PATH})
+    set(_FFmpeg_os_params --cc=${CMAKE_C_COMPILER} --cxx=${CMAKE_CXX_COMPILER})
+endif()
 
-  # Should we try to point ffmpeg at zlib if we are building it?
-  # Currently it uses the system version.
-  if (fletch_ENABLE_Zlib)
-    list(APPEND ffmpeg_DEPENDS ZLib)
-  endif()
-
-  set (FFmpeg_patch ${fletch_SOURCE_DIR}/Patches/FFmpeg/${_FFmpeg_version})
-  if (EXISTS ${FFmpeg_patch})
-    set(FFMPEG_PATCH_COMMAND ${CMAKE_COMMAND}
-      -DFFmpeg_patch:PATH=${FFmpeg_patch}
-      -DFFmpeg_source:PATH=${fletch_BUILD_PREFIX}/src/FFmpeg
-      -P ${FFmpeg_patch}/Patch.cmake
-      )
-  else()
-    set(FFMPEG_PATCH_COMMAND "")
-  endif()
-
-  if (BUILD_SHARED_LIBS)
-    set(_shared_lib_params --enable-shared --disable-static)
-  else()
-    set(_shared_lib_params --disable-shared
-                           --enable-static
-                           --enable-pic
-                           --extra-cflags=-fPIC
-                           --extra-cxxflags=-fPIC
-                           --disable-asm
-       )
-  endif()
-  set(FFMPEG_CONFIGURE_COMMAND
-    PKG_CONFIG_PATH=${fletch_BUILD_INSTALL_PREFIX}/lib/pkgconfig
+set(FFMPEG_CONFIGURE_COMMAND
+    ${FFMPEG_SET_PKG_CONFIG}
     ${fletch_BUILD_PREFIX}/src/FFmpeg/configure
     --prefix=${fletch_BUILD_INSTALL_PREFIX}
     --enable-runtime-cpudetect
@@ -92,37 +56,44 @@ else ()
     ${_FFmpeg_yasm}
     ${_FFmpeg_zlib}
     ${_shared_lib_params}
-    --cc=${CMAKE_C_COMPILER}
-    --cxx=${CMAKE_CXX_COMPILER}
+    ${_FFmpeg_os_params}
     --enable-rpath
-    )
+)
 
-  if (_FFmpeg_version VERSION_LESS 3.3.0)
+if (_FFmpeg_version VERSION_LESS 3.3.0)
     # memalign-hack is only needed for windows and older versions of ffmpeg
     list(APPEND FFMPEG_CONFIGURE_COMMAND --enable-memalign-hack)
     # bzlib errors if not found in newer versions (previously it did not)
     list(APPEND FFMPEG_CONFIGURE_COMMAND --enable-bzlib)
     list(APPEND FFMPEG_CONFIGURE_COMMAND --enable-outdev=sdl)
-  endif()
+endif()
 
-  if(APPLE)
+include(External_yasm)
+set(fletch_YASM ${fletch_BUILD_PREFIX}/src/yasm-build/yasm)
+set(_FFmpeg_yasm --yasmexe=${fletch_YASM})
+list(APPEND ffmpeg_DEPENDS yasm)
+
+# Should we try to point ffmpeg at zlib if we are building it?
+# Currently it uses the system version.
+if (fletch_ENABLE_Zlib)
+    list(APPEND ffmpeg_DEPENDS ZLib)
+endif()
+
+if(APPLE)
     list(APPEND FFMPEG_CONFIGURE_COMMAND --sysroot=${CMAKE_OSX_SYSROOT} --disable-doc)
-  endif()
+endif()
 
-
-  Fletch_Require_Make()
-  ExternalProject_Add(FFmpeg
+ExternalProject_Add(FFmpeg
     URL ${FFmpeg_file}
     DEPENDS ${ffmpeg_DEPENDS}
     URL_MD5 ${FFmpeg_md5}
     ${COMMON_EP_ARGS}
     PATCH_COMMAND ${FFMPEG_PATCH_COMMAND}
     CONFIGURE_COMMAND ${FFMPEG_CONFIGURE_COMMAND}
-    BUILD_COMMAND ${MAKE_EXECUTABLE}
-    INSTALL_COMMAND ${MAKE_EXECUTABLE} install
-    )
-  fletch_external_project_force_install(PACKAGE FFmpeg)
-endif ()
+    BUILD_COMMAND ${FFMPEG_BUILD_COMMAND}
+    INSTALL_COMMAND ${FFMPEG_INSTALL_COMMAND}
+)
+fletch_external_project_force_install(PACKAGE FFmpeg)
 
 set(FFmpeg_ROOT ${fletch_BUILD_INSTALL_PREFIX} CACHE PATH "" FORCE)
 
