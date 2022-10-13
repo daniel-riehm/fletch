c42fe61dc2270d2b0713b6890394d4d76a9c86f6
Daniel Riehm <daniel.riehm@kitware.com>
2022-10-13 16:21:58 -0400

    Patch pybind11 to define ssize_t for MSVC

--- Patches/pybind11/include/pybind11/numpy.h
+++ Patches/pybind11/include/pybind11/numpy.h
@@ -27,6 +27,7 @@
 #if defined(_MSC_VER)
 #  pragma warning(push)
 #  pragma warning(disable: 4127) // warning C4127: Conditional expression is constant
+using ssize_t = SSIZE_T;
 #endif
 
 /* This will be true on all flat address space platforms and allows us to reduce the
